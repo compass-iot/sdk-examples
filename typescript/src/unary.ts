ba9a13dafc920db96b76f95bf611c42fa2c2997d
Dennis Wirya <dennis@compassiot.com.au>
2023-11-15 12:27:26 +1100

    Working unary

---
+++ typescript/src/unary.ts
@@ -0,0 +1,40 @@
+import { AggregateByPathRequest } from "@buf/compassiot_model.bufbuild_es/platform/v1/unary_pb"
+import { DayOfWeek } from "@buf/compassiot_model.bufbuild_es/compass/v1/time_pb"
+
+import createGatewayClient from "./client"
+
+const SECRET = "...insert client secret..."
+
+const request = new AggregateByPathRequest({
+  linestringWkt: "LINESTRING (151.194525 -33.87363, 151.194109 -33.873076, 151.193736 -33.872564, 151.193507 -33.872265, 151.193347 -33.872052, 151.193259 -33.871925, 151.193228 -33.871889)",
+
+  // Try October:
+  // - Dates: 2023-10-01 to 2023-10-31
+  // - All hours (0 - 23)
+  // - Only weekends (Saturday & Sunday)
+  dateTimeRange: {
+    start: {
+      day: 1,
+      // beware JS & TS users, Date.getMonth() returns integer between 0-11, so should add +1
+      month: 10,
+      year: 2023
+    },
+    end: {
+      day: 14,
+      month: 10,
+      year: 2023
+    },
+    dayOfWeek: [
+      DayOfWeek.SATURDAY,
+      DayOfWeek.SUNDAY
+    ],
+    hourOfDay: []
+  }
+})
+
+const client = createGatewayClient(SECRET)
+
+console.log("Querying AggregateByPath")
+client.aggregateByPath(request).then(response => {
+  console.log(response.toJsonString({ prettySpaces: 2 }))
+})
