78afdf30d02fd4ca85c5e81ae33b320386eacfbf
Dennis Wirya <dennis@compassiot.com.au>
2023-12-13 09:24:35 +0700

    Unary shim

--- python/client.py
+++ python/client.py
@@ -1,30 +1,92 @@
+import asyncio
+import inspect
+import requests
+from collections import abc
 from typing import Any, Callable
 
 from grpc import ClientCallDetails, RpcError, intercept_channel, secure_channel, ssl_channel_credentials
-from grpc_interceptor.client import ClientInterceptor, ClientCallDetails, ClientInterceptorReturnType
+from grpc_interceptor.client import ClientInterceptor, ClientCallDetails
 
 from compassiot.gateway.v1.gateway_pb2 import AuthenticateRequest
 from compassiot.gateway.v1.gateway_pb2_grpc import ServiceStub
 
 
 HOST = "beta.api.compassiot.cloud"
-SECRET = "__insert_your_api_key__"
+SECRET = "__INSERT_YOUR_COMPASSIOT_API_KEY__"
 
 
 def create_gateway_client() -> ServiceStub:
-	interceptors = [AccessTokenInterceptor(HOST, SECRET)]
+	# UnaryRestInterceptor must be last as it's the layer which makes the API call,
+	# unlike AccessTokenInterceptor which just populates the header
+	interceptors = [AccessTokenInterceptor(HOST, SECRET), UnaryRestInterceptor(HOST)]
 	channel = secure_channel(HOST, ssl_channel_credentials())
 	channel = intercept_channel(channel, *interceptors)
 	return ServiceStub(channel)
 
 
+class UnaryRestInterceptor(ClientInterceptor):
+	"""
+	Shim to convert unary gRPC calls to pure REST, due to several suspected regressions in
+	core gRPC library:
+	- https://github.com/grpc/grpc/issues/29706
+	- https://github.com/grpc/grpc/issues/33935
+	"""
+
+	_HTTP_HEADERS = {"content-type": "application/proto"}
+
+	@staticmethod
+	def _build_deserializer_map():
+		with secure_channel("mock", ssl_channel_credentials()) as channel:
+			mock_service = ServiceStub(channel)
+			stub_method_tuples = list(filter(lambda member: not member[0].startswith("__"), inspect.getmembers(mock_service)))
+			map = {}
+			for (k, v) in stub_method_tuples:
+				map[k] = v._response_deserializer
+			return map
+
+	def __init__(self, host: str):
+		self.host = host
+		self.deserializer_map = self._build_deserializer_map()
+
+	def _call_rest(self, request: Any, call_details: ClientCallDetails):
+		url = "https://%s/%s" % (self.host, call_details.method)
+
+		# Copy headers
+		headers = self._HTTP_HEADERS.copy()
+		if call_details.metadata is not None:
+			for (k, v) in call_details.metadata:
+				headers[k] = v
+
+		# Make request & deserialize it
+		response = requests.post(url, data=request.SerializeToString(True), headers=headers, auth=call_details.credentials)
+		response.raise_for_status()
+
+		# Wrap into future
+		rpc = call_details.method.split("/")[-1]
+		deserializer = self.deserializer_map[rpc]
+		future = asyncio.get_event_loop().create_future()
+		future.set_result(deserializer(response.content))
+		return future
+			
+
+	def intercept(self, method: Callable[..., Any], request_or_iterator: Any, call_details: ClientCallDetails):
+		# Check if streaming: if yes, avoid proxy
+		if isinstance(request_or_iterator, abc.Iterator):
+			return method(request_or_iterator, call_details)
+		# Else, make REST request
+		return self._call_rest(request_or_iterator, call_details)
+
+
 class AccessTokenInterceptor(ClientInterceptor):
 	def __init__(self, host: str, secret: str) -> None:
-		channel = secure_channel(host, ssl_channel_credentials())
-		service = ServiceStub(channel)
-		self.access_token = service.Authenticate(AuthenticateRequest(token=secret)).access_token
 		self.host = host
 		self.secret = secret
+
+		interceptors = [UnaryRestInterceptor(host)]
+		with intercept_channel(secure_channel(host, ssl_channel_credentials()), *interceptors) as channel:
+			service = ServiceStub(channel)
+			response = service.Authenticate(AuthenticateRequest(token=secret))
+			self.access_token = response.access_token			
 
 	@staticmethod
 	def _create_details_with_auth(call_details: ClientCallDetails, access_token: str) -> ClientCallDetails:
@@ -37,7 +99,7 @@ class AccessTokenInterceptor(ClientInterceptor):
 			call_details.compression,
 		)
 	
-	def intercept(self, method: Callable[..., Any], request_or_iterator: Any, call_details: ClientCallDetails) -> ClientInterceptorReturnType:
+	def intercept(self, method: Callable[..., Any], request_or_iterator: Any, call_details: ClientCallDetails):
 		try:
 			return method(request_or_iterator, self._create_details_with_auth(call_details, self.access_token))
 		except RpcError as error:
--- python/unary.py
+++ python/unary.py
@@ -1,47 +1,23 @@
-import requests
-
-from google.protobuf.message import Message
 from google.protobuf.empty_pb2 import Empty
 
-from compassiot.gateway.v1.gateway_pb2 import AuthenticateRequest, AuthenticateResponse
-from compassiot.location.v1.location_pb2 import ReverseGeocodeRequest, ReverseGeocodeResponse
-from compassiot.native.v1.native_pb2 import GetVehiclesResponse
-from client import HOST, SECRET
-
-
-METHOD_PREFIX = "/compassiot.gateway.v1.Service"
-HTTP_HEADERS = {"content-type": "application/proto"}
-
-
-def call_unary(host: str, secret: str, method: str, request: Message):
-	# Get access token first
-	auth_url = "https://%s/%s/%s" % (host, METHOD_PREFIX, "Authenticate")
-	auth_req = AuthenticateRequest(token=secret).SerializeToString(True)
-	auth_future = requests.post(auth_url, data=auth_req, headers=HTTP_HEADERS)
-	auth_res = AuthenticateResponse.FromString(auth_future.content)
-
-	# Attach access token to header
-	headers = HTTP_HEADERS.copy()
-	headers["Authorization"] = "Bearer %s" % auth_res.access_token
-
-	url = "https://%s/%s/%s" % (host, METHOD_PREFIX, method.strip("/"))
-	response = requests.post(url, data=request.SerializeToString(True), headers=headers)
-	return response.content
+from compassiot.location.v1.location_pb2 import ReverseGeocodeRequest
+from client import create_gateway_client
 
 
 def main():
+	client = create_gateway_client()
+
 	request = ReverseGeocodeRequest(polygon_wkt="POLYGON ((151.3614711724905 -33.42188098251515, 151.34152608798246 -33.4171079590409, 151.3416655640977 -33.402554438990045, 151.36216855306685 -33.39603367115148, 151.37974254361256 -33.41151969574161, 151.3614711724905 -33.42188098251515))")
-	bytes = call_unary(HOST, SECRET, "ReverseGeocode", request)
-	response = ReverseGeocodeResponse.FromString(bytes)
+	response = client.ReverseGeocode(request)
 	print("ReverseGeocode:")
 	print(response.name)
 	print()
 
 	request = Empty()
-	bytes = call_unary(HOST, SECRET, "NativeGetVehicles", request)
-	response = GetVehiclesResponse.FromString(bytes)
+	response = client.NativeGetVehicles(request)
 	print("NativeGetVehicles:")
 	print(response.provider_get)
 
+
 if __name__ == "__main__":
-  main()
+	main()
