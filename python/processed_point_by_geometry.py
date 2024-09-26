da02d2a0da6e7a064627370b027d5b9e8842711b
sujithsimon22 <sujith@compassiot.com.au>
2024-09-26 13:45:28 +1000

    add support for new fields in processed point

--- python/processed_point_by_geometry.py
+++ python/processed_point_by_geometry.py
@@ -4,6 +4,7 @@ import grpc
 import compassiot.gateway.v1.gateway_pb2_grpc as gateway
 import compassiot.compass.v1.time_pb2 as time
 import compassiot.platform.v1.streaming_pb2 as streaming
+import compassiot.compass.v1.vehicle_pb2 as vehicle
 
 
 def paginate_processed_point(client: gateway.ServiceStub, req: streaming.ProcessedPointByGeometryRequest):
@@ -27,8 +28,8 @@ def main():
     client = create_gateway_client()
 
     request = streaming.ProcessedPointByGeometryRequest(
-        linestring_or_polygon_wkt="POLYGON ((151.116490002445 -34.136652437446564, 150.76133775468065 -33.970878990313345,150.8238645588653 -33.68832143811975, 150.93391173422782 -33.51542003355166, 151.17151359012706 -33.594622738528074, 151.3165757758332 -33.67583417052261, 151.3115736314981 -33.80478175152794, 151.22903824997502 -34.03515466768564, 151.116490002445 -34.136652437446564))",
-        date_time_range= time.DateTimeRange(
+        linestring_or_polygon_wkt="LINESTRING(151.18703722810923 -33.8695894847834,151.18361472940623 -33.868689747746664)",
+        date_time_range=time.DateTimeRange(
             start=time.LocalDate(
                 day=1,
                 month=10,
@@ -41,11 +42,35 @@ def main():
             ),
             day_of_week=[
                 time.DayOfWeek.SATURDAY,
-                time.DayOfWeek.SUNDAY
+                time.DayOfWeek.SUNDAY,
+                time.DayOfWeek.MONDAY,
+                time.DayOfWeek.TUESDAY,
             ],
-            hour_of_day=[i for i in range(24)]  # 0 to 23
+            hour_of_day=[i for i in range(24)],  # 0 to 23
+            # include the list of dates you would like to exclude from the query, if any
+            exclude_date=[
+                time.LocalDate(
+                    day=10,
+                    month=10,
+                    year=2023
+                ),
+                time.LocalDate(
+                    day=11,
+                    month=10,
+                    year=2023
+                ),
+            ]
         ),
-        filters=[streaming.RawRequestFilter.UNSPECIFIED]
+        # specify if any filter exists, leave empty for all data
+        filters=[streaming.RawRequestFilter.UNSPECIFIED],
+        # specify all vehicle types that you would like to receive in the response, leave empty for all vehicle types
+        vehicle_type_filter=[vehicle.VehicleType.CAR, vehicle.VEHICLE_TYPE_UNSPECIFIED, vehicle.VehicleType.BUS,
+        vehicle.VehicleType.HV, vehicle.VehicleType.VAN, vehicle.VehicleType.TRUCK, vehicle.VehicleType.LCV,
+        vehicle.VehicleType.LV, vehicle.VehicleType.CRANE, vehicle.VehicleType.TRACTOR,
+        vehicle.VehicleType.TRAILER, vehicle.VehicleType.LOADER, vehicle.VehicleType.MOTORCYCLE,
+        vehicle.VehicleType.GARBAGE_TRUCK, vehicle.VehicleType.MICROBUS, vehicle.VehicleType.SUV,
+        vehicle.VehicleType.LOADER, vehicle.VehicleType.PICKUP_TRUCK]
+
     )
 
     for response in paginate_processed_point(client, request):
